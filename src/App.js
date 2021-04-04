26eec62e2d256c6048c634873d586a6fc1b5fd50
Yash Seeta <yashseeta94@gmail.com>
2021-04-04 14:40:49 +0200

    set attribute to true for Tab when location matches its link

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTruck, faUserCircle } from "@fortawesome/free-solid-svg-icons";
-import { Tab, Tabs, TabList } from "react-tabs";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
-import { RecoilRoot } from "recoil";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import {
+    BrowserRouter,
+    Route,
+    Switch,
+    Link,
+    useLocation,
+} from "react-router-dom";
+import { RecoilRoot, useRecoilState } from "recoil";
+import { useState, useEffect } from "react";
 
 import "react-confirm-alert/src/react-confirm-alert.css";
 import "./styles/App.css";
@@ -11,15 +18,28 @@ import "./styles/profile.css";
 import ProfilePage from "./Pages/ProfilePage";
 import PackageListPage from "./Pages/PackageListPage";
 import QueryResultsPage from "./Pages/QueryResultsPage";
+import { tabIndexData } from "./states/tabIndexData";
 
 function App() {
+    const [tabIndex, setTabIndex] = useState(0);
+    /* {window.location.pathname === "/" && setTabIndex(0)};
+    console.log(`tab index is ${tabIndex}`);
+    console.log(`current location is ${window.location.pathname}`); */
     return (
         <div className="App">
             <RecoilRoot>
                 <BrowserRouter>
-                    <Tabs className="tabs">
+                    <Tabs
+                        className="tabs"
+                        selectedIndex={tabIndex}
+                        onSelect={(index) => setTabIndex(index)}
+                    >
                         <TabList>
-                            <Tab>
+                            <Tab
+                                data-selected={
+                                    window.location.pathname === "/profile" || window.location.pathname === "/"
+                                }
+                            >
                                 <Link to="/profile">
                                     <FontAwesomeIcon
                                         icon={faUserCircle}
@@ -28,7 +48,11 @@ function App() {
                                     />
                                 </Link>
                             </Tab>
-                            <Tab>
+                            <Tab
+                                data-selected={
+                                    window.location.pathname === "/packagelist"
+                                }
+                            >
                                 <Link to="/packagelist">
                                     <FontAwesomeIcon
                                         icon={faTruck}
@@ -38,6 +62,8 @@ function App() {
                                 </Link>
                             </Tab>
                         </TabList>
+                        <TabPanel></TabPanel>
+                        <TabPanel></TabPanel>
                     </Tabs>
                     <Switch>
                         <Route path="/" exact component={ProfilePage} />
---
+++ src/states/tabIndexData.js
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const tabIndexData = atom({
+    key: "tabIndexData",
+    default: 0,
+});
