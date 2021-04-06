a9af4837d1684b9cf81f4bffa7b32c28f058d390
Yash Seeta <yashseeta94@gmail.com>
2021-04-06 17:14:57 +0200

    Add local functions to get selected tab

--- src/components/TabLocal.jsx
+++ src/components/TabLocal.jsx
@@ -1,39 +1,43 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTruck, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import {Link} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
 export default function TabLocal() {
     const [tabIndex, setTabIndex] = useState(0);
+    const location = useLocation();
+
+    function checkPathToProfile() {
+        return location.pathname === "/profile" || location.pathname === "/";
+    }
+
+    function checkPathToPackageList() {
+        return location.pathname === "/packagelist";
+    }
     return (
         <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
-        <TabList>
-            <Tab
-                data-selected={
-                    window.location.pathname === "/profile" ||
-                    window.location.pathname === "/"
-                }
-            >
-                <Link to="/profile">
-                    <FontAwesomeIcon
-                        icon={faUserCircle}
-                        fontSize="xxx-large"
-                        className="fa fa-profile"
-                    />
-                </Link>
-            </Tab>
-            <Tab data-selected={window.location.pathname === "/packagelist"}>
-                <Link to="/packagelist">
-                    <FontAwesomeIcon
-                        icon={faTruck}
-                        fontSize="xxx-large"
-                        className="fa fa-tracking"
-                    />
-                </Link>
-            </Tab>
-        </TabList>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-    </Tabs>
-    )
+            <TabList>
+                <Tab data-selected={checkPathToProfile()}>
+                    <Link to="/profile">
+                        <FontAwesomeIcon
+                            icon={faUserCircle}
+                            fontSize="xxx-large"
+                            className="fa fa-profile"
+                        />
+                    </Link>
+                </Tab>
+                <Tab data-selected={checkPathToPackageList()}>
+                    <Link to="/packagelist">
+                        <FontAwesomeIcon
+                            icon={faTruck}
+                            fontSize="xxx-large"
+                            className="fa fa-tracking"
+                        />
+                    </Link>
+                </Tab>
+            </TabList>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+        </Tabs>
+    );
 }
